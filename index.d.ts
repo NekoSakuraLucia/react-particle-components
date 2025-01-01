ec24767c59d86858193b65f34ad7567181a886b1
NekoSakuraLucia <mokurolucia@gmail.com>
2025-01-01 14:08:30 +0700

    Change from using straight types Let's use it separately for convenience.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,12 @@
+type ParticleType = {
+    id?: string;
+    type?: "circle" | "bubbles";
+}
+
 declare module "react-particle-components" {
     import { FC } from "react";
 
-    const ParticleComponent: FC<{ id?: string }>;
+    const ParticleComponent: FC<ParticleType>;
 
     export default ParticleComponent;
 }

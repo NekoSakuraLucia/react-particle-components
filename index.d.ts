07a513c852d5e9c208dc1c4b6a40060a3ec93e87
NekoSakuraLucia <mokurolucia@gmail.com>
2025-01-01 12:25:22 +0700

    Add type definitions for TypeScript that user want to use.

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module "react-particle-components" {
+    import { FC } from "react";
+
+    const ParticleComponent: FC<{ id: string | undefined }>;
+
+    export default ParticleComponent;
+}
